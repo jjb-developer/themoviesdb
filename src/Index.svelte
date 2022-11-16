21921d42ca1cc4c9e555ff2fb4dc855998ef37f2
Jose Pereira <developer2022@gmail.com>
2022-11-16 10:09:38 -0400

    update porque en mi smartphone no se veia bien el responsive

--- src/Index.svelte
+++ src/Index.svelte
@@ -18,7 +18,7 @@
    }*/
 
 </script>
-<div class='relative w-full bg-[#032541]'>
+<div class='relative w-full bg-[#032541] overflow-hidden'>
    <MenuPrincipal/>
    <Header movie={preview}/>
    <Slider title={"acción"} movies={movies.action}/>
@@ -27,8 +27,8 @@
    <Slider title={"ficción"} movies={movies.science}/>
    <Slider title={"historía"} movies={movies.history}/>
    <Slider title={"documentales"} movies={movies.documentary}/>
-   <div class='p-20'>
-      <img src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' alt='logo'>
+   <div class='w-full h-40 flex items-center justify-center border-t border-[#F75C03]'>
+      <img class='h-10' src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' alt='logo'>
    </div>
-   <div class='w-full h-24'></div>
+   <div class='w-full h-12'></div>
 </div>
